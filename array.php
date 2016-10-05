fdce25983aff2925d3ae983319b9b1e7749d1b65
Mochamad Fathoni <tunathoni@gmail.com>
2016-10-05 10:07:29 +0700

    a

--- array.php
+++ array.php
@@ -4,6 +4,8 @@ $arr1 = array(
 	1,
 	2,
 	3,
+	4,
+	5,
 );
 
 $arr2 = [
@@ -16,13 +18,15 @@ $arr2 = [
 
 // echo count($arr2);
 
-$jmlArr = count($arr2);
+// $jmlArr = count($arr2);
 
-for ($i = 0; $i < $jmlArr; $i++ ) {
-	echo $arr2[$i] . "\n";
-}
+// for ($i = 0; $i < $jmlArr; $i++ ) {
+// 	echo $arr2[$i] . "\n";
+// }
 
+$out = array_slice($arr1, 0, -2); // yang dibuang 1 node dari kiri
 
+print_r($out);
 
 
 
