038eb7571ab2cf234452b896290c908620d098d1
Mochamad Fathoni <tunathoni@gmail.com>
2016-10-11 14:31:53 +0700

    a

--- implements.php
+++ implements.php
@@ -4,6 +4,7 @@ interface Payment{
 	public function kredit();
 	public function debit();
 	public function getATM();
+	public function getSaldo();
 }
 
 class ATM 
@@ -13,28 +14,38 @@ class ATM
 		$bank->getATM();		
 	}
 
+	public function cetakSaldo(Payment $bank)
+	{
+		$bank->getSaldo();
+	}
+
 }
 
 class Mandiri implements Payment
 {
 	public function __construct()
 	{
-		$this->kredit = 0;
+		$this->saldo = 0;
 	}
 
 	public function kredit()
 	{
-		$this->kredit++;
+		$this->saldo++;
 	}
 
 	public function debit()
 	{
-		$this->kredit--;
+		$this->saldo--;
 	}
 
 	public function getATM()
 	{
 		echo "Ini dari bank Mandiri \n";
+	}
+
+	public function getSaldo()
+	{
+		echo "Saldo rekening Mandiri Anda : " . $this->saldo . " \n";
 	}
 	
 }
@@ -43,32 +54,51 @@ class BNI implements Payment
 {
 	public function __construct()
 	{
-		$this->kredit = 0;
+		$this->saldo = 0;
 	}
 
 	public function kredit()
 	{
-		$this->kredit++;
+		$this->saldo++;
 	}
 
 	public function debit()
 	{
-		$this->kredit--;
+		$this->saldo--;
 	}
 
 	public function getATM()
 	{
 		echo "Ini dari bank BNI \n";
 	}
+
+	public function getSaldo()
+	{
+		echo "Saldo rekening BNI Anda : " . $this->saldo . " \n";
+	}
 	
 }
 
 $mandiri = new Mandiri();
+$mandiri->kredit();
+$mandiri->kredit();
+$mandiri->kredit();
+$mandiri->kredit();
+
 $bni = new BNI();
+$bni->kredit();
+$bni->kredit();
+$bni->kredit();
+$bni->kredit();
+$bni->debit();
+
 
 $atm = new ATM();
 
 $atm->inisialAtm($mandiri);
+$atm->cetakSaldo($mandiri);
+
 $atm->inisialAtm($bni);
+$atm->cetakSaldo($bni);
 
 ?>
