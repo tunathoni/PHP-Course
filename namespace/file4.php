717f250c156df716974c1e55d058647573837843
Mochamad Fathoni <tunathoni@gmail.com>
2016-10-13 09:43:40 +0700

    a

--- namespace/file4.php
+++ namespace/file4.php
@@ -3,19 +3,23 @@
  include 'file1.php';
  include 'file2.php';
  include 'file3.php';
+ include 'file6.php';
  include 'lib/file5.php';
 
  use foo as veline;
+ use foo2 as veline2;
  use bar as bro;
  use animate;
  use lib\man as kodew;
 
  $cat 		= new veline\Cat;
+ $cat2 		= new veline2\Cat;
  $woman 	= new kodew\Woman;
  $anjing 	= new bro\Dog;
 
  echo $cat->says();
  echo $cat::eat();
+ echo $cat2->says();
  echo $anjing->says();
  echo animate\Animal::breathe();
 
---
+++ namespace/file6.php
@@ -0,0 +1,12 @@
+<?php namespace foo2;
+
+class Cat 
+{
+	
+	public function says()
+	{
+		echo "Hello cat \n";
+	}
+}
+
+?>
