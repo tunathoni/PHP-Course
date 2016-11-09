880156e9cdd5ba2e7a07c2148a54de817d7cfcb7
Mochamad Fathoni <tunathoni@gmail.com>
2016-11-09 11:04:37 +0700

    session, login, flashdata, templating, custom controller

---
+++ new_ci/application/views/template/menu_samping.php
@@ -0,0 +1,14 @@
+<div class="col-sm-3 col-md-2 sidebar">
+  <ul class="nav nav-sidebar">
+    <li class="active">
+      <a href="<?=site_url('welcome')?>">
+        Dashboard <span class="sr-only">(current)</span>
+      </a>
+    </li>
+    <li>
+      <a href="<?=site_url('siswa')?>">
+        Siswa
+      </a>
+    </li>
+  </ul>
+</div>
